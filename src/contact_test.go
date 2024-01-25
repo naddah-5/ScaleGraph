5ad8d973455285c091e28ca9e5dc673eaec13be6
Anders Dahl <andersoskardahl@hotmail.com>
2024-01-25 15:46:01 +0100

    reformatted test to follow similar structure

--- src/contact_test.go
+++ src/contact_test.go
@@ -7,54 +7,60 @@ import (
 )
 
 func TestValidateIPNegative(t *testing.T) {
+	var testName string = "TestValidateIPNegative"
 	var invalidIP string = "-127.0.0.1"
 	err := validateIPStructure(invalidIP)
 	if err == nil {
-		fmt.Println("[TestValidateIPNegative] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName,  err.Error())
 		t.FailNow()
 	}
 }
 
 func TestValidateIPLarge(t *testing.T) {
+	var testName string = "TestValidateIPLarge"
 	var invalidIP string = "1270.0.0.1"
 	err := validateIPStructure(invalidIP)
 	if err == nil {
-		fmt.Println("[TestValidateIPLarge] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName, err.Error())
 		t.FailNow()
 	}
 }
 
 func TestValidateIPText(t *testing.T) {
+	var testName string = "TestValidateIPText"
 	var invalidIP string = "127.zero.0.1"
 	err := validateIPStructure(invalidIP)
 	if err == nil {
-		fmt.Println("[TestValidateIPText] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName, err.Error())
 		t.FailNow()
 	}
 }
 
 func TestValidateIPTextManyFaults(t *testing.T) {
+	var testName string = "TestValidateIPTextManyFaults"
 	var invalidIP string = "1270.zero.0.-1.10"
 	err := validateIPStructure(invalidIP)
 	if err == nil {
-		fmt.Println("[TestValidateIPTextManyFaults] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName, err.Error())
 		t.FailNow()
 	}
 }
 
 func TestValidateUDPPortNegative(t *testing.T) {
+	var testName string = "TestValidateUDPPortNegative"
 	var negativePort int = -80
 	err := validateUDPPort(negativePort)
 	if err == nil {
-		fmt.Println("[TestValidateUDPPortNegative] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName, err.Error())
 	}
 }
 
 func TestValidateUDPPortLARGE(t *testing.T) {
+	var testName string = "TestValidateUDPPortLarge"
 	var largePort int = 8080
 	err := validateUDPPort(largePort)
 	if err == nil {
-		fmt.Println("[TestValidateUDPPortLarge] \n" + err.Error())
+		fmt.Printf("[%s] - %s\n", testName, err.Error())
 	}
 }
 
@@ -64,9 +70,9 @@ func TestCreateContact(t *testing.T) {
 	var expectedIP string = "127.0.0.1"
 	var expectedPort int = 80
 	var expectedID [5]uint32 = *new([5]uint32)
-	newContact, conErr := BuildContact(expectedIP, expectedPort, expectedID)
-	if conErr != nil {
-		errMsg = errMsg + "could not create new contact " + conErr.Error() + "\n"
+	newContact, err := BuildContact(expectedIP, expectedPort, expectedID)
+	if err != nil {
+		errMsg = errMsg + "could not create new contact " + err.Error() + "\n"
 	}
 	if newContact.IP() != expectedIP {
 		errMsg = errMsg + "IP missmatch: expected - " + expectedIP + " received - " + newContact.nodeIP + "\n"
--- src/id_test.go
+++ src/id_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestGenerateIDRandomness(t *testing.T) {
+	var testName string = "TestGenerateIDRandomness"
 	var baseID [5]uint32
 	baseID, _ = GenerateID()
 	var confidence float64 = 1000000
@@ -18,117 +19,128 @@ func TestGenerateIDRandomness(t *testing.T) {
 		}
 	}
 	if coincidence/confidence >= warning {
-		fmt.Printf("[TestGenerateIDRandomness] - WARNING: low probability event: %d, collisions in %d generations", int(coincidence), int(confidence))
+		fmt.Printf("[%s] - WARNING: low probability event: %d, collisions in %d generations", testName, int(coincidence), int(confidence))
 		t.FailNow()
 	}
 }
 
 func TestRelativeDistance(t *testing.T) {
+	var testName string = "TestRelativeDistance"
 	var idA [5]uint32 = [5]uint32{1, 2, 3, 4, 5} // 1 + 1 + 2 + 1 + 2
 	var idB [5]uint32 = [5]uint32{0, 0, 0, 0, 0}
 	var expectedDist = 7
 	res := RelativeDistance(&idA, &idB)
 	if res != expectedDist {
-		fmt.Printf("[TestRelativeDistance] - found relative distnace %v, expected relative distance %v\n", res, expectedDist)
+		fmt.Printf("[%s] - found relative distnace %v, expected relative distance %v\n", testName, res, expectedDist)
 		t.FailNow()
 	}
 }
 
 func TestRelativeDistancePointing(t *testing.T) {
+	var testName string = "TestRelativeDistancePointing"
 	var idA [5]uint32 = [5]uint32{1, 2, 3, 4, 5}
 	var idB [5]uint32 = [5]uint32{0, 0, 0, 0, 0}
 	RelativeDistance(&idA, &idB)
 	if idA[0] != 1 {
+		fmt.Printf("[%s] - expected a relative distance of 1, found %d", testName, idA[0])
 		t.FailNow()
 	}
 }
 
 func TestHammingDistance(t *testing.T) {
+	var testName string = "TestHammingDistance"
 	var pointA uint32 = uint32(0b10101010101010101)
 	var pointB uint32 = uint32(0b01010101010101010)
 	var expectedHamDist int = 17
 	res := hammingDistance(pointA, pointB)
 	if res != expectedHamDist {
-		fmt.Printf("[TestHammingDistance] - found hamming distance %v, expected hamming distance %v\n", res, expectedHamDist)
+		fmt.Printf("[%s] - found hamming distance %v, expected hamming distance %v\n", testName, res, expectedHamDist)
 		t.FailNow()
 	}
 }
 
 func TestHammingDistance1(t *testing.T) {
+	var testName string = "TestHammingdistance1"
 	var pointA uint32 = uint32(0b10101010010101)
 	var pointB uint32 = uint32(0b01010101010101010)
 	var expectedHamDist int = 7
 	res := hammingDistance(pointA, pointB)
 	if res != expectedHamDist {
-		fmt.Printf("[TestHammingDistance1] - found hamming distance %v, expected hamming distance %v\n", res, expectedHamDist)
+		fmt.Printf("[%s] - found hamming distance %v, expected hamming distance %v\n", testName, res, expectedHamDist)
 		t.FailNow()
 	}
 }
 
 func TestHammingDistance2(t *testing.T) {
+	var testName string = "TestHammingDistance2"
 	var pointA uint32 = uint32(0b1)
 	var pointB uint32 = uint32(0b0)
 	var expectedHamDist int = 1
 	res := hammingDistance(pointA, pointB)
 	if res != expectedHamDist {
-		fmt.Printf("[TestHammingDistance1] - found hamming distance %v, expected hamming distance %v\n", res, expectedHamDist)
+		fmt.Printf("[%s] - found hamming distance %v, expected hamming distance %v\n", testName, res, expectedHamDist)
 		t.FailNow()
 	}
 }
 
 func TestHammingDistance3(t *testing.T) {
+	var testName string = "TestHammingDistance3"
 	var pointA uint32 = 213
 	var pointB uint32 = 42
 	var expectedHamDist int = 8
 	res := hammingDistance(pointA, pointB)
 	if res != expectedHamDist {
-		fmt.Printf("[TestHammingDistance3] - found hamming distance %v, expected hamming distance %v\n", res, expectedHamDist)
+		fmt.Printf("[%s] - found hamming distance %v, expected hamming distance %v\n", testName, res, expectedHamDist)
 		t.FailNow()
 	}
 }
 
 func TestHammingDistance4(t *testing.T) {
+	var testName string = "TestHammingDistance4"
 	var pointA uint32 = 0
 	var tmp int = -1
 	var pointB uint32 = uint32(tmp)
 	var expectedHamDist int = 32
 	res := hammingDistance(pointA, pointB)
 	if res != expectedHamDist {
-		fmt.Printf("[TestHammingDistance4] - found hamming distance %v, expected hamming distance %v\n", res, expectedHamDist)
+		fmt.Printf("[%s] - found hamming distance %v, expected hamming distance %v\n", testName, res, expectedHamDist)
 		t.FailNow()
 	}
 }
 
 func TestPrefixMatch(t *testing.T) {
+	var testName string = "TestPrefixMatch"
 	var idA [5]uint32 = [5]uint32{0, 1, 2, 3, 4}
 	var idB [5]uint32 = [5]uint32{1, 2, 3, 4, 5}
 	const expected int = 31
 	prefixMatch := distPrefixLength(idA, idB)
 	if prefixMatch != expected {
-		fmt.Printf("[TestPrefixMatch] - expected prefix length of %d, received %d\n", expected, prefixMatch)
+		fmt.Printf("[%s] - expected prefix length of %d, received %d\n", testName, expected, prefixMatch)
 		t.FailNow()
 	}
 
 }
 
 func TestPrefixMatch1(t *testing.T) {
+	var testName string = "TestPrefixMatch1"
 	var idA [5]uint32 = [5]uint32{0, 1, 2, 3, 4}
 	var idB [5]uint32 = [5]uint32{0, 2, 3, 4, 5}
 	const expected int = 62
 	prefixMatch := distPrefixLength(idA, idB)
 	if prefixMatch != expected {
-		fmt.Printf("[TestPrefixMatch] - expected prefix length of %d, received %d", expected, prefixMatch)
+		fmt.Printf("[%s] - expected prefix length of %d, received %d", testName, expected, prefixMatch)
 		t.FailNow()
 	}
 }
 
 func TestPrefixMatch2(t *testing.T) {
+	var testName string = "TestPrefixMatch2"
 	var idA [5]uint32 = [5]uint32{0, 0, 0, 0, 0}
 	var idB [5]uint32 = [5]uint32{0, 0, 0, 0, 0}
 	prefixMatch := distPrefixLength(idA, idB)
 	const expected int = 160
 	if prefixMatch != expected {
-		fmt.Printf("[TestPrefixMatch] - expected prefix length of %d, received %d", expected, prefixMatch)
+		fmt.Printf("[%s] - expected prefix length of %d, received %d", testName, expected, prefixMatch)
 		t.FailNow()
 	}
 }
