5d5d38ca67ba81dbc753c5bd92fd56ddf4a9689f
Anders Dahl <andersoskardahl@hotmail.com>
2024-03-08 13:18:44 +0100

    mv

--- src/network.go
+++ src/network.go
@@ -10,11 +10,15 @@ type handler struct {
 	set map[[5]uint32]chan RPC
 }
 
+// Initializes and returns a new handler.
 func NewHandler(buffer int) handler {
 	ch := make(map[[5]uint32]chan RPC, buffer)
 	return handler{ch}
 }
 
+// Creates a RPC id handle corresponding to the given RPC id.
+// Returns the response channel.
+// Note that this can over write existing handles.
 func (handler *handler) Add(id [5]uint32) chan RPC {
 	respChan := make(chan RPC)
 	// potential (low probability) collisions, would not break but deletes old channel
@@ -27,6 +31,8 @@ func (handler *handler) Add(id [5]uint32) chan RPC {
 	return respChan
 }
 
+// Returns the response channel for a given RPC id.
+// Returns an error if there is no matching RPC id.
 func (handler *handler) Retrieve(id [5]uint32) (chan RPC, error) {
 	respChan, ok := handler.set[id]
 	if !ok {
@@ -35,6 +41,8 @@ func (handler *handler) Retrieve(id [5]uint32) (chan RPC, error) {
 	return respChan, nil
 }
 
+// Removes the given id from the active RPC map.
+// If there is no match, does nothing.
 func (handler *handler) Drop(id [5]uint32) {
 	delete(handler.set, id)
 }
@@ -56,6 +64,8 @@ func NewNetwork(ln chan RPC, sn chan RPC, servIP [4]byte) network {
 	return newNetwork
 }
 
+// Sends a RPC and creates a corresponding RPC id handle.
+// Returns an error if the response eceedes the timeout.
 func (net *network) Send(rpc RPC) (RPC, error) {
 	if DEBUG {
 		log.Printf("%+v: sending rpc: %+v, to IP: %+v\n", rpc.Sender.id, rpc, rpc.Receiver)
@@ -75,6 +85,8 @@ func (net *network) Send(rpc RPC) (RPC, error) {
 	return rpc, nil
 }
 
+// Start a listener on the network channel.
+// Returns an error if the channel closes.
 func (net *network) Listen(node *Node) error {
 	for {
 		rpc, ok := <-net.listener
--- src/network_test.go
+++ src/network_test.go
@@ -1,16 +1,22 @@
 package scalegraph
 
 import (
+	"log"
 	"testing"
 )
 
 func TestHandler(t *testing.T) {
+	VERBOSE := true
+
 	in := make(chan RPC)
 	out := make(chan RPC)
 	net := NewNetwork(in, out, [4]byte{0, 0, 0, 0})
-	input := make([]RPC, 100)
+	input := make([]RPC, 10)
 	for range input {
 		id := GenerateID()
 		net.handler.Add(id)
+	}
+	if VERBOSE {
+		log.Printf("handler is:")
 	}
 }
