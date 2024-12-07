0ffc6c0381154c2297aade02870413cf87cc5d00
Anders Dahl <andersoskardahl@hotmail.com>
2024-12-07 11:39:37 +0100

    mv

--- src/controller.go
+++ src/controller.go
@@ -5,10 +5,26 @@ import "main/src/kademlia"
 func (node *Node) InputLoop() {
 	for {
 		rpc := <-node.controller
-		
+		node.handler(rpc)
+	}
+}
+
+func (node *Node) handler(rpc kademlia.RPC) {
+	switch rpc.CMD {
+	case kademlia.PING:
+		node.ping(rpc)
+	case kademlia.STORE:
+		node.store(rpc)
 	}
 }
 
 func (node *Node) ping(rpc kademlia.RPC) {
+	node.AddContact(rpc.Sender)
+	resp := kademlia.GenerateResponse(rpc.ID, node.Contact)
+	
+
+}
+
+func (node *Node) store(rpc kademlia.RPC) {
 
 }
--- src/kademlia/rpc.go
+++ src/kademlia/rpc.go
@@ -1,7 +1,5 @@
 package kademlia
 
-import ()
-
 type cmd int
 
 const (
@@ -35,6 +33,34 @@ func (c cmd) String() string {
 
 type RPC struct {
 	ID       [5]uint32
+	CMD      cmd
 	Response bool
+	Sender   Contact
 	Receiver [4]byte
+}
+
+// Generate a fresh send RPC, for a response RPC use GenerateResponse instead.
+func GenerateRPC(sender Contact) RPC {
+	rpc := RPC{
+		ID: RandomID(),
+		Response: false,
+		Sender: sender,
+	}
+	return rpc
+}
+
+// Generates a fresh response RPC.
+func GenerateResponse(id [5]uint32, sender Contact) RPC {
+	rpc := RPC{
+		ID: id,
+		Response: true,
+		Sender: sender,
+	}
+	return rpc
+}
+
+// Set a RPC as a ping.
+func (rpc *RPC) ping(receiver [4]byte) {
+	rpc.CMD = PING
+	rpc.Receiver = receiver
 }
